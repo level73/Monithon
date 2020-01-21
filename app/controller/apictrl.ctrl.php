6b0cb05ac5f217d04cf52392c74a83d685e1fcd5
Yavin <tech@alanzard.com>
2020-01-21 11:27:55 +0100

    API report status switcher

--- app/controller/apictrl.ctrl.php
+++ app/controller/apictrl.ctrl.php
@@ -23,15 +23,38 @@
         if(is_numeric($report_id)){
           $Report = new Report;
           $report = $Report->find($report_id);
-          $response['code'] = 200;
-          $response['message'] = 'Ok';
-          $response['body'] = array(
-            'report_id' => $report_id,
-            'report_url' => APPURL . '/report/' . $report_id,
-            'report_title' => $report->titolo,
-            'report_assessment' => $report->giudizio_sintetico,
-            'report_status' => $report->status
-          );
+          if(!empty($report)){
+            // Cycle Status Values
+            $status = null;
+            switch($report->status){
+              case 1:
+                $status = 'Bozza';
+                break;
+              case 3:
+                $status = 'In attesa di revisione';
+                break:
+              case 5:
+                $status = 'In revisione';
+                break;
+              case 7:
+                $status = 'Pubblicato';
+                break;
+            }
+
+            $response['code'] = 200;
+            $response['message'] = 'Ok';
+            $response['body'] = array(
+              'report_id' => $report_id,
+              'report_url' => APPURL . '/report/' . $report_id,
+              'report_title' => $report->titolo,
+              'report_assessment' => $report->giudizio_sintetico,
+              'report_status' => $status
+            );
+          }
+          else {
+            $response['code'] = 404;
+            $response['message'] = 'Report non trovato.';
+          }
         }
       }
       else {
