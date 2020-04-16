cd074d27bf79317096b4ba5fb8f1c03941ce97d0
IronRunes <tech@alanzard.com>
2020-04-16 08:11:03 +0200

    Singolo report

--- app/view/report/view.php
+++ app/view/report/view.php
@@ -85,7 +85,10 @@
                     <img src="<?php echo image($image); ?>" class="img-fluid">
                 </div>
             <?php } ?>
-            <?php foreach($report->videos as $video){ ?>
+            <?php
+            foreach($report->videos as $video){
+                dbga($video);
+            ?>
             <div class="embed-responsive embed-responsive-16by9">
                 <iframe class="embed-responsive-item" src="<?php echo $video->embed; ?>?rel=0" allowfullscreen></iframe>
             </div>
