6f1fafffa20a4b562256c8bded6a50750dc41a69
IronRunes <code@level73.it>
2020-05-04 06:47:56 +0200

    Emailer Class - removed test

--- scripts/emailer_test.php
+++ scripts/emailer_test.php
@@ -1,9 +1,10 @@
 <?php
+/*
 define('ROOT', $_SERVER['DOCUMENT_ROOT']);
 define('DS', '/');
 // Load configuration files
 require(ROOT . DS . 'config' . DS . 'local.php');
 require(ROOT . DS . 'config' . DS . 'system.php');
     require '../lib/emailer.class.php';
-    $Emailer = new Emailer();
+    $Emailer = new Emailer(); */
 
