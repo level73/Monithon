3a4922b32ced90768a5d36a37208a01e76466131
IronRunes <tech@alanzard.com>
2020-05-26 07:38:09 +0200

    Step buttons

--- app/view/report/create.php
+++ app/view/report/create.php
@@ -541,7 +541,7 @@
         <div class="form-group">
           <div class="custom-control custom-checkbox">
             <input class="custom-control-input" type="checkbox" value="<?php echo PENDING_REVIEW; ?>" id="status" name="status">
-            <label class="custom-control-label" for="status">Il Report è pronto (<strong>completo in step 1 e step 2</strong>) per essere revisionato dalla Redazione, che lo pubblicherà se rispetterà i nostri <a href="#">Termini d'uso</a></label>
+            <label class="custom-control-label" for="status">Il Report è pronto (<strong>completo in step 1, step 2 e step 3</strong>) per essere revisionato dalla Redazione, che lo pubblicherà se rispetterà i nostri <a href="#">Termini d'uso</a></label>
           </div>
 
         </div>
