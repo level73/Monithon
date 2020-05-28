9ec105be67c3bada834d97d83b177e8d7d5bc2ea
IronRunes <code@level73.it>
2020-05-28 07:42:20 +0200

    Rimoss riferimento TOS nell'approvazione report/tab, issue #79

--- app/view/report/edit.php
+++ app/view/report/edit.php
@@ -630,7 +630,7 @@
                 </div>
                 <div class="custom-control custom-checkbox">
                     <input class="custom-control-input" type="checkbox" value="<?php echo PENDING_REVIEW; ?>" id="status_tab_3" name="status_tab_3">
-                    <label class="custom-control-label" for="status_tab_3">Questo step del report (Impatto & Risultati) è pronto per essere revisionato dalla Redazione, che lo pubblicherà se rispetterà i nostri <a href="#">Termini d'uso</a></label>
+                    <label class="custom-control-label" for="status_tab_3">Questo step del report (Impatto & Risultati) è pronto per essere revisionato dalla Redazione</label>
                 </div>
             </fieldset>
         </div>
@@ -639,7 +639,7 @@
         <div class="form-group">
           <div class="custom-control custom-checkbox">
             <input class="custom-control-input" type="checkbox" value="<?php echo PENDING_REVIEW; ?>" id="status" name="status">
-              <label class="custom-control-label" for="status">Il Report è pronto (<strong>completo in step 1 e step 2</strong>) per essere revisionato dalla Redazione, che lo pubblicherà se rispetterà i nostri <a href="#">Termini d'uso</a></label>
+              <label class="custom-control-label" for="status">Il Report è pronto (<strong>completo in step 1 e step 2</strong>) per essere revisionato dalla Redazione</label>
           </div>
 
         </div>
