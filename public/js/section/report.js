6417c2068459e46c5b8e2e77e21238a0958fa86c
IronRunes <tech@alanzard.com>
2020-10-15 12:31:17 +0200

    Changed Mapbox tile API to address issue, this time in report pages #81

--- public/js/section/report.js
+++ public/js/section/report.js
@@ -12,22 +12,18 @@ var MonithonReport = {
 
         var LocationMap = L.map('report-map').setView([lat,lon], 12);
 
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            tileSize: 512,
             maxZoom: 18,
-            id: 'mapbox.streets',
+            zoomOffset: -1,
+            id: 'mapbox/streets-v11',
             accessToken: 'pk.eyJ1IjoibHZsNzMiLCJhIjoiY2swdzllbHd4MDB1ejNpbW1tMXk0eHY5MSJ9.7pGmC5RA3CQQYsgXLknlZg'
         }).addTo(LocationMap);
 
         new L.marker([lat, lon], { draggable: true} ).addTo(LocationMap);
 
-
-
-        console.log(lat + ', ' + lon);
     },
-
-
-
 }
 
 MonithonReport.init();
