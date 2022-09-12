1d6d892b38212695d0abc0cca7fea039fe511d86
IronRunes <code@level73.it>
2022-09-12 16:25:08 +0200

    Show/Hide button for Gender box

--- app/view/foot.php
+++ app/view/foot.php
@@ -5,7 +5,7 @@
       <script src="/public/js/vendor/bootstrap.min.js"></script>
       <script src="/public/js/vendor/bootstrap-select.min.js"></script>
       <script src="/public/js/vendor/bootstrap-table.min.js"></script>
-      <script src="/public/js/main.min.js?v3.0"></script>
+      <script src="/public/js/main.min.js?v3.2"></script>
       <?php if( isset($street_map) && $street_map == true ){ ?>
       <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js"
               integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og=="
