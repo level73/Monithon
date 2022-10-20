041db58d4dcfa1a083c7bec540d5114d6a9aed26
IronRunes <code@level73.it>
2022-10-20 09:49:23 +0200

    Prettify Landing/Login page

--- app/view/main/index.php
+++ app/view/main/index.php
@@ -2,8 +2,7 @@
   <div class="row">
     <div class="col">
         <div clasS="main-box-wrap">
-      <h1>MONITHON v3.0</h1>
-      <h2>Benvenuto!</h2>
+      <h1>Benvenuto!</h1>
 
         <p>Questa è la piattaforma di lavoro di Monithon per la creazione e l’invio dei report di monitoraggio civico.</p>
         <p>Vai su “<a href="/report/create">Nuovo Report</a>” per creare un report di monitoraggio. Per attivare la guida MoniTutor, incolla la URL della pagina del progetto che hai scelto su OpenCoesione nel primo campo.<p>
