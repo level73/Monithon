7dea30236310a438eb7e14f8d05ea797e207a405
IronRunes:Yavin <code@level73.it>
2025-04-15 14:08:24 +0200

    una cosa per i pdf

--- app/controller/imonitorctrl.ctrl.php
+++ app/controller/imonitorctrl.ctrl.php
@@ -795,13 +795,17 @@ class ImonitorCtrl extends Ctrl
 
 
             $html .= "<style>
-                            @font-face{
+                            /*@font-face{
                                 font-family: 'IBM Plex Sans';
                                 font-style: normal;
                                 font-weight: 400;
                                 src: url('/public/font/IBMPlexSans/IBMPlexSans-Regular.ttf') format('ttf');
                             }
                             h1, h2, h3, h4, h5, h6, p, span, li, dl, td, th, div { font-family: 'IBM Plex Sans', sans-serif; }
+                            
+                             */
+                             * { font-family: DejaVu Sans; }
+                             h1, h2, h3, h4, h5, h6, p, span, li, dl, td, th, div { font-family: DejaVu Sans; }
                             td { padding: 2pt; margin-bottom: 3px; border-bottom: 1px dotted #AAAAAA; }
                             p, span, td { font-size: 12pt; }    
                             .value_green { color: green; }
